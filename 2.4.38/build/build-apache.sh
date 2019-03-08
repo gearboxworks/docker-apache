dd6b4415e0a421ca7bc43172529150e3d1fc666b
MickMake <mick@mickmake.com>
2019-03-08 17:29:34 +1100

    Simplification.

--- 2.4.38/build/build-apache.sh
+++ 2.4.38/build/build-apache.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-APKS="apache2=2.4.38-r3 apache2-proxy=2.4.38-r3 apache2-ssl=2.4.38-r3 apache2-utils=2.4.38-r3 apache2-icons=2.4.38-r3"
+# See gearboxworks/gearbox-base for details.
+test -f /build/include-me.sh && . /build/include-me.sh
 
-apk update && apk add --no-cache ${APKS}
+c_ok "Started."
+
+c_ok "Installing packages."
+APKS="apache2=2.4.38-r3 apache2-proxy=2.4.38-r3 apache2-ssl=2.4.38-r3 apache2-utils=2.4.38-r3 apache2-icons=2.4.38-r3"
+apk update && apk add --no-cache ${APKS}; checkExit
 
 if [ ! -d /run/apache2 ]
 then
 	mkdir -p /run/apache2
 fi
 
-if [ -d /build/rootfs ]
-then
-	rsync -HvaxP /build/rootfs/ /
-fi
-
+c_ok "Finished."
