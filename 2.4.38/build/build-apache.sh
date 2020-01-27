653d8b853eeb640a9a38d1adc8255096ed99a72f
MickMake <github@mickmake.com>
2020-01-27 21:51:30 +1100

    Release commit.

--- 2.4.38/build/build-apache.sh
+++ 2.4.38/build/build-apache.sh
@@ -6,7 +6,7 @@ test -f /build/include-me.sh && . /build/include-me.sh
 c_ok "Started."
 
 c_ok "Installing packages."
-APKS="apache2=2.4.38-r3 apache2-proxy=2.4.38-r3 apache2-ssl=2.4.38-r3 apache2-utils=2.4.38-r3 apache2-icons=2.4.38-r3"
+APKS="apache2=2.4.41-r0 apache2-proxy=2.4.41-r0 apache2-ssl=2.4.41-r0 apache2-utils=2.4.41-r0 apache2-icons=2.4.41-r0"
 apk update && apk add --no-cache ${APKS}; checkExit
 
 if [ ! -d /run/apache2 ]
