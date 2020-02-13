813c6b3b3f57a955912803991eeb26e735e97616
MickMake <github@mickmake.com>
2020-02-13 17:13:34 +1100

    New build env.

--- build/build-apache.sh
+++ build/build-apache.sh
@@ -6,7 +6,7 @@ test -f /build/include-me.sh && . /build/include-me.sh
 c_ok "Started."
 
 c_ok "Installing packages."
-APKS="apache2=2.4.35-r0 apache2-proxy=2.4.35-r0 apache2-ssl=2.4.35-r0 apache2-utils=2.4.35-r0 apache2-icons=2.4.35-r0"
+APKS="$(cat /build/build-apache.apks)"
 apk update && apk add --no-cache ${APKS}; checkExit
 
 if [ ! -d /run/apache2 ]
